8d34ddd2354b3eaf9b1530c3d91d9308bb7397df
Alexxreal <160492192+Alexxreal@users.noreply.github.com>
2024-09-30 16:05:49 +0000

    hello

--- javascript.js
+++ javascript.js
@@ -1,6 +1,16 @@
 let currentPos = [4, 5]
 let mousePos = [0, 0]
+let boundBox = document.getElementById("eyeball").getBoundingClientRect()
+let eyeballPosMid
+let eyeballPos
+let eyeballStrength = 0.15
+let eyeStrength = 0.05
+let gridSize = 12
 const keys = [["w", 0, -12],["s", 0, 12],["a", -12, 0],["d", 12, 0]];
+
+const round = (number, step) => {
+  return Math.round(number / step) * step;
+};
 
 function sleep(milliseconds) {
     var start = new Date().getTime();
@@ -11,24 +21,35 @@ function sleep(milliseconds) {
     }
   }
 
-function randDir() {
-    let randNum = Math.random()
-    if (randNum >= 0.5) {
-        return 12
-    }
-    else {
-        return -12
-    }
-}
-
 document.addEventListener('mousemove', function(e){
+  eyeballPosMid = [document.getElementById("eyeball").getBoundingClientRect().x + (document.getElementById("eyeball").getBoundingClientRect().width / 2), document.getElementById("eyeball").getBoundingClientRect().y + (document.getElementById("eyeball").getBoundingClientRect().height / 2)]
+  eyeballPos = [document.getElementById("eyeball").getBoundingClientRect().x, document.getElementById("eyeball").getBoundingClientRect().y]
+  let eyeballGridPos = [0,0]
+  let eyeGridPos = [0,0]
   var browserHeight = [screen.availWidth, screen.availHeight]
   var x = e.clientX;
   var y = e.clientY;
-  document.getElementById("pointer").style.transform = "translate("+x+"px, "+y+"px)";
+  /* document.getElementById("pointer").style.transform = "translate("+x+"px, "+y+"px)"; */
+  /* fix this shit */
   mousePos = [x, y]
-  console.log(document.getElementById("eyeball").getBoundingClientRect())
+  eyeballGridPos[0] = round((calculatePos(mousePos[0] ,mousePos[1])[0] * eyeballStrength), gridSize)
+  eyeballGridPos[1] = round((calculatePos(mousePos[0] ,mousePos[1])[1] * eyeballStrength), gridSize)
+  eyeGridPos[0] = round((calculatePos(mousePos[0] ,mousePos[1])[0] * eyeStrength), gridSize)
+  eyeGridPos[1] = round((calculatePos(mousePos[0] ,mousePos[1])[1] * eyeStrength), gridSize)
+  document.getElementById("eyeball").style.transform = "translate("+ eyeballGridPos[0] +"px, "+ eyeballGridPos[1] +"px)";
+  document.getElementById("eyeTrans").style.transform = "translate("+ eyeGridPos[0] +"px, "+ eyeGridPos[1] +"px)";
+  console.log(document.getElementById("eyeball").style.transform = "translate("+ eyeballGridPos[0] +"px, "+ eyeballGridPos[1] +"px)")
 });
+
+function calculatePos(oriPosX, oriPosY) {
+  let posY
+  let posX
+ 
+  posY = oriPosY - eyeballPosMid[1]
+  posX = oriPosX - eyeballPosMid[0]
+  
+  return [posX ,posY];
+}
 
 function myFunction(event) {
   
@@ -38,8 +59,6 @@ function myFunction(event) {
       currentPos[0] += keys[i][1]
       currentPos[1] += keys[i][2]
       document.getElementById("eyeball").style.transform = "translate("+currentPos[0]+"px, "+currentPos[1]+"px)"; 
-      console.log(mousePos)
-      console.log(currentPos)
     }
   }
 }
