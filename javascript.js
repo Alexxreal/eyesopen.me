4b16c320fae690a7b0d1ebf7ae7e308dee97f724
Alexxreal <30265417@bsdc.ac.uk>
2024-10-18 12:17:07 +0100

    a

--- javascript.js
+++ javascript.js
@@ -12,15 +12,6 @@ const round = (number, step) => {
   return Math.round(number / step) * step;
 };
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-  }
-
 document.addEventListener('mousemove', function(e){
   eyeballPosMid = [document.getElementById("eyeball").getBoundingClientRect().x + (document.getElementById("eyeball").getBoundingClientRect().width / 2), document.getElementById("eyeball").getBoundingClientRect().y + (document.getElementById("eyeball").getBoundingClientRect().height / 2)]
   eyeballPos = [document.getElementById("eyeball").getBoundingClientRect().x, document.getElementById("eyeball").getBoundingClientRect().y]
@@ -32,10 +23,12 @@ document.addEventListener('mousemove', function(e){
   /* document.getElementById("pointer").style.transform = "translate("+x+"px, "+y+"px)"; */
   /* fix this shit */
   mousePos = [x, y]
-  eyeballGridPos[0] = round((calculatePos(mousePos[0] ,mousePos[1])[0] * eyeballStrength), gridSize)
-  eyeballGridPos[1] = round((calculatePos(mousePos[0] ,mousePos[1])[1] * eyeballStrength), gridSize)
-  eyeGridPos[0] = round((calculatePos(mousePos[0] ,mousePos[1])[0] * eyeStrength), gridSize)
-  eyeGridPos[1] = round((calculatePos(mousePos[0] ,mousePos[1])[1] * eyeStrength), gridSize)
+  
+  for (let i = 0; i < eyeballGridPos.length; i++) {
+    eyeballGridPos[i] = round((calculatePos(mousePos[0] ,mousePos[1])[i] * eyeballStrength), gridSize)
+    eyeGridPos[i] = round((calculatePos(mousePos[0] ,mousePos[1])[i] * eyeStrength), gridSize)
+  }
+
   document.getElementById("eyeball").style.transform = "translate("+ eyeballGridPos[0] +"px, "+ eyeballGridPos[1] +"px)";
   document.getElementById("eyeTrans").style.transform = "translate("+ eyeGridPos[0] +"px, "+ eyeGridPos[1] +"px)";
   console.log(document.getElementById("eyeball").style.transform = "translate("+ eyeballGridPos[0] +"px, "+ eyeballGridPos[1] +"px)")
@@ -49,16 +42,4 @@ function calculatePos(oriPosX, oriPosY) {
   posX = oriPosX - eyeballPosMid[0]
   
   return [posX ,posY];
-}
-
-function myFunction(event) {
-  
-  let key = event.key;
-  for (let i = 0; i < keys.length; i++) {
-    if (key == keys[i][0]) {
-      currentPos[0] += keys[i][1]
-      currentPos[1] += keys[i][2]
-      document.getElementById("eyeball").style.transform = "translate("+currentPos[0]+"px, "+currentPos[1]+"px)"; 
-    }
-  }
 }
